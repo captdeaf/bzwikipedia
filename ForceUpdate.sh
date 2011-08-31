715dbe55427e1ee929a6d5ee080d76b184528e2a
Greg Millam <captdeaf@gmail.com>
2011-08-31 08:35:36 -0500

    Added a sleep to ForceUpdate in case somebody runs it by accident.

--- ForceUpdate.sh
+++ ForceUpdate.sh
@@ -4,6 +4,11 @@
 
 cd "`dirname $0`"
 
+echo "About to remove pdata/bzwikipedia.dat in 10 seconds"
+echo "Press Ctrl+c to cancel."
+
+sleep 10
+
 mv pdata/bzwikipedia.dat pdata/bzwikipedia.dat.old
 
 exec sh StartWiki.sh
