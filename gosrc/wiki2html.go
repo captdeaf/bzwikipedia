9b55a3baf5c956ee88686f0d2fd59c8f35dc11ed
Greg Millam <captdeaf@gmail.com>
2011-09-15 02:36:10 -0700

    More slogging along on wiki2html

--- gosrc/wiki2html.go
+++ gosrc/wiki2html.go
@@ -21,6 +21,7 @@ type markupInfo struct {
   refCount int
   refNames map[string] int
   refs []string
+  inCode bool
 }
 
 type token struct {
@@ -33,11 +34,21 @@ var entityFinds = regexp.MustCompile("<|>|&")
 func unparseEntities(input string) string {
   return entityFinds.ReplaceAllStringFunc(input, func(what string) string {
     switch what {
-    case "&": return "&amp;";
-    case ">": return "&gt;";
-    case "<": return "&lt;";
+    case "&": return "&amp;"
+    case ">": return "&gt;"
+    case "<": return "&lt;"
     }
     return what
+  })
+}
+
+var matchuri = regexp.MustCompile("(http|https|ftp)://[^ \\t\\n]*(\\.[^ \\t\\n\\.]*)*")
+
+func parsePlainText(input string) string {
+  input = unparseEntities(input)
+
+  return matchuri.ReplaceAllStringFunc(input, func(what string) string {
+    return fmt.Sprintf("<a href=\"%s\">%s</a>", what, what)
   })
 }
 
@@ -46,13 +57,13 @@ var entityReplace = regexp.MustCompile("&(#?[a-z0-9]+);")
 func parseEntities(input string) string {
   return entityReplace.ReplaceAllStringFunc(input, func(what string) string {
     switch what {
-    case "&lt;": return "<";
-    case "&gt;": return ">";
-    case "&amp;": return "&";
-    case "&#93;": return "]";
-    case "&#92;": return "\\";
-    case "&#91;": return "[";
-    case "&quot;": return "\"";
+    case "&lt;": return "<"
+    case "&gt;": return ">"
+    case "&amp;": return "&"
+    case "&#93;": return "]"
+    case "&#92;": return "\\"
+    case "&#91;": return "["
+    case "&quot;": return "\""
     }
     // TODO: Handle "all" escape codes. But as this is doubly encoded (for
     // XML) and UTF-8, that might not be necessary. Also, Wikipedia is
@@ -61,7 +72,7 @@ func parseEntities(input string) string {
   })
 }
 
-var wikitokens = regexp.MustCompile("\\n|\\n\\*|\\n#|\\{\\{|\\}\\}|\\[\\[|\\]\\]|\\[|\\]|'''''|'''|''|=====|====|===|==|<source[^>]*>|</source>|<ref[^>]*>|</ref>|<code[^>]*>|</code>")
+var wikitokens = regexp.MustCompile("\\n\\*|\\n#|\\n|\\{\\{|\\}\\}|\\[|\\]|'''''|'''|''|=====|====|===|==|<source[^>]*>|</source>|<ref[^>]*>|</ref>|<code[^>]*>|</code>")
 
 func tokenize(input []byte) []token {
   // Find the location of all known tokens.
@@ -114,17 +125,75 @@ func tokenize(input []byte) []token {
   return allTokens
 }
 
+func renderTemplate(tname string, namedArgs map[string]string, argv []string) string {
+  lname := strings.ToLower(tname)
+  content := strings.Join(argv," ")
+  switch lname {
+  case "as of":
+    return fmt.Sprintf(
+        "%s %s",
+        tname, content)
+  case "see also":
+    return fmt.Sprintf(
+        "(%s: <i><a href=\"/wiki/%s\">%s</a></i>)",
+        tname, content, content)
+  case "cquote":
+    return fmt.Sprintf(
+        "<blockquote>%s</blockquote>",
+        content)
+  case "sic":
+    return "<span class=\"sic\">[<a href=\"/wiki/Sic\">Sic</a>]</span>"
+  case "refbegin":
+    return "<ol>"
+  case "refend":
+    return "</ol>"
+  case "citation":
+    return fmt.Sprintf(
+        "\"%s\" by %s, %s",
+        namedArgs["title"], namedArgs["last1"], namedArgs["first1"])
+  }
+  return "FOO"
+}
+
+var namedArg = regexp.MustCompile("^ *([a-zA-Z0-9]+) *= *(.*) *$")
 // {{ ... }}
 func parseTemplate(input []byte, tokens []token, i int, mi *markupInfo) (string, int) {
   // fmt.Printf("Entering {{...\n")
   // defer fmt.Printf("Leaving }}\n")
-  _, eidx := parseGeneral(input, tokens, i + 1, []string{"}}"}, mi)
-  return "--template--", eidx
+  body, eidx := parseGeneral(input, tokens, i + 1, []string{"}}"}, mi)
+  args := strings.Split(body, "|")
+  tname := strings.TrimSpace(args[0])
+  namedArgs := map[string] string {}
+  positionalArgs := []string{}
+
+  for i := 1; i < len(args); i++ {
+    arg := args[i]
+    if strings.Contains(arg, "=") {
+      matches := namedArg.FindStringSubmatch(arg)
+      if matches != nil {
+        namedArgs[strings.ToLower(matches[1])] = matches[2]
+        continue
+      }
+    }
+    positionalArgs = append(positionalArgs, arg)
+  }
+
+  result := renderTemplate(tname, namedArgs, positionalArgs)
+  if result == "FOO" {
+    return fmt.Sprintf("{{%s}}", body), eidx
+  }
+  return result, eidx
 }
 
 func parseExternalLink(input []byte, tokens []token, i int, mi *markupInfo) (string, int) {
   // fmt.Printf("Entering [...\n")
   // defer fmt.Printf("Leaving ]\n")
+  // We only recurse if it looks like we're followed by an http.
+  if len(tokens) > (i+1) {
+    if len(tokens[i+1].Val) < 7 || tokens[i+1].Val[0:7] != "http://" {
+      return "[", i
+    }
+  }
   body, eidx := parseGeneral(input, tokens, i + 1, []string{"]"}, mi)
   args := strings.SplitN(body, " ", 2)
   var title string
@@ -134,7 +203,7 @@ func parseExternalLink(input []byte, tokens []token, i int, mi *markupInfo) (str
   } else {
     title = page
   }
-  link := fmt.Sprintf("<a href=\"%s\">%s</a>", page, title)
+  link := fmt.Sprintf("<a class=\"external\" href=\"%s\">%s</a>", page, title)
   return link, eidx
 }
 
@@ -142,7 +211,13 @@ func parseExternalLink(input []byte, tokens []token, i int, mi *markupInfo) (str
 func parseInternalLink(input []byte, tokens []token, i int, mi *markupInfo) (string, int) {
   // fmt.Printf("Entering [[...\n")
   // defer fmt.Printf("Leaving ]]\n")
-  body, eidx := parseGeneral(input, tokens, i + 1, []string{"]]"}, mi)
+
+  // Internal link won't have any markup inside of it. At least, it better not!
+  if len(tokens) < (i+2) || tokens[i+2].Val != "]" || tokens[i+3].Val != "]" {
+    return "[[", i
+  }
+
+  body, eidx := parseGeneral(input, tokens, i + 1, []string{"]","]"}, mi)
   args := strings.SplitN(body, "|", 2)
   var title string
   page := args[0]
@@ -151,11 +226,11 @@ func parseInternalLink(input []byte, tokens []token, i int, mi *markupInfo) (str
   } else {
     title = page
   }
-  link := fmt.Sprintf("<a href=\"/wiki/%s\">%s</a>", page, title)
+  link := fmt.Sprintf("<a class=\"internal\" href=\"/wiki/%s\">%s</a>", page, title)
   return link, eidx
 }
 
-// [[ ... ]]
+// <ref> ... </ref>
 func parseReference(input []byte, tokens []token, i int, mi *markupInfo) (string, int) {
   start := i
   // fmt.Printf("Entering %s...\n", tokens[start].Val)
@@ -174,7 +249,7 @@ func parseReference(input []byte, tokens []token, i int, mi *markupInfo) (string
   body, eidx := parseGeneral(input, tokens, i + 1, []string{"</ref>"}, mi)
   mi.refCount++
   link := fmt.Sprintf("<a href=\"#ref%d\">[%d]</a>", mi.refCount, mi.refCount)
-  mi.refs = append(mi.refs, fmt.Sprintf("<a tag=\"#ref%d\"></a>%s", body))
+  mi.refs = append(mi.refs, fmt.Sprintf("<a tag=\"#ref%d\"></a>%s", mi.refCount, body))
   return link, eidx
 }
 
@@ -184,6 +259,10 @@ func parseHeader(input []byte, tokens []token, i int, mi *markupInfo) (string, i
   x := len(tokens[start].Val)
   // fmt.Printf("Entering %s...\n", tokens[start].Val)
   // defer fmt.Printf("Leaving %s\n", tokens[start].Val)
+
+  if len(tokens) < (i+2) || tokens[i+2].Val != tokens[start].Val {
+    return tokens[start].Val, i
+  }
 
   body, eidx := parseGeneral(input, tokens, i + 1, []string{tokens[start].Val}, mi)
 
@@ -196,6 +275,10 @@ func parseMarkup(input []byte, tokens []token, i int, mi *markupInfo) (string, i
   x := len(tokens[start].Val)
   // fmt.Printf("Entering %s...\n", tokens[start].Val)
   // defer fmt.Printf("Leaving %s\n", tokens[start].Val)
+
+  if len(tokens) < (i+2) || tokens[i+2].Val != tokens[start].Val {
+    return tokens[start].Val, i
+  }
 
   body, eidx := parseGeneral(input, tokens, i + 1, []string{tokens[start].Val}, mi)
 
@@ -210,37 +293,37 @@ func parseMarkup(input []byte, tokens []token, i int, mi *markupInfo) (string, i
   return fmt.Sprintf("<span style=\"color:yellow;\">%s</span>", body), eidx
 }
 
-// ''''' ... '''''
+// ...
 func parseCode(input []byte, tokens []token, i int, mi *markupInfo, end string) (string, int) {
-  // fmt.Printf("Entering %s...\n", tokens[start].Val)
-  // defer fmt.Printf("Leaving %s\n", tokens[start].Val)
-  results := []string{}
+  // fmt.Printf("Entering %s...\n", tokens[i].Val)
+  // defer fmt.Printf("Leaving %s\n", end)
 
-  i += 1
-  for {
-    if i >= len(tokens) { break }
-    if tokens[i].Val == end { break }
-    if tokens[i].IsToken && len(tokens[i].Val) > 5 && tokens[i].Val[0:5] == "<code" {
-      results = append(results, tokens[i].Val)
-      s, eidx := parseCode(input, tokens, i, mi, "</code>")
-      results = append(results, s)
-      results = append(results, "</code>")
-      i = eidx
-    } else if tokens[i].IsToken && len(tokens[i].Val) > 7 && tokens[i].Val[0:7] == "<source" {
-      results = append(results, tokens[i].Val)
-      s, eidx := parseCode(input, tokens, i, mi, "</source>")
-      results = append(results, s)
-      results = append(results, "</source>")
-      i = eidx
-    }
-    results = append(results, tokens[i].Val)
-    i++
-  }
+  oldinCode := mi.inCode
+  mi.inCode = true
+  defer func() { mi.inCode = oldinCode }()
+
+  body, eidx := parseGeneral(input, tokens, i + 1, []string{end}, mi)
 
   if (end == "</code>") {
-    return fmt.Sprintf("<tt>%s</tt>", unparseEntities(strings.Join(results, ""))), i
+    return fmt.Sprintf("<tt>%s</tt>", body), eidx
   }
-  return fmt.Sprintf("<pre>%s</pre>", unparseEntities(strings.Join(results, ""))), i
+  return fmt.Sprintf("<pre>%s</pre>", body), eidx
+}
+
+func doesListContinue(tokens []token, ltype string, i int) bool {
+  for {
+    i++
+    if i >= len(tokens) { return false }
+    if tokens[i].IsToken {
+      switch tokens[i].Val {
+      case ltype: return true
+      case "\n#": return false
+      case "\n*": return false
+      case "\n":  return false
+      }
+    }
+  }
+  return false
 }
 
 // Token parsers return the string value of their contents, and the next index
@@ -260,9 +343,17 @@ func parseGeneral(input []byte, tokens []token, start int, endtokens []string, m
   for ;; {
     if i >= len(tokens) { break }
     if tokens[i].IsToken {
-      for j := 0; j < len(endtokens); j++ {
-        if tokens[i+j].IsToken && tokens[i+j].Val == endtokens[j] {
-          return strings.Join(results, ""), i
+      if len(endtokens) > 0 && (i + len(endtokens)) <= len(tokens) {
+        doret := true
+        var j int
+        for j = 0; j < len(endtokens); j++ {
+          if tokens[i+j].Val != endtokens[j] {
+            doret = false
+            break
+          }
+        }
+        if doret {
+          return strings.Join(results, ""), i + len(endtokens) - 1
         }
       }
       switch {
@@ -271,25 +362,42 @@ func parseGeneral(input []byte, tokens []token, start int, endtokens []string, m
           results = append(results, fmt.Sprintf("</%s>", listType))
           listType = ""
         }
-        results = append(results, "<br />")
+        if (i+1) < len(tokens) && tokens[i+1].IsToken && tokens[i+1].Val == "\n" {
+          if mi.inCode {
+            results = append(results, "\n\n")
+          } else {
+            results = append(results, "\n<br />\n<br />")
+          }
+          i++
+        } else {
+          results = append(results, "\n")
+        }
       case tokens[i].Val == "\n*":
-        if listType != "ul" {
+        if !mi.inCode && listType != "ul" && doesListContinue(tokens, "\n*", i) {
           results = append(results, "<ul>")
           listType = "ul"
         }
-        results = append(results, "<li>")
+        if listType != "" {
+          results = append(results, "<li>")
+        } else {
+          results = append(results, tokens[i].Val)
+        }
       case tokens[i].Val == "\n#":
-        if listType != "ol" {
+        if !mi.inCode && listType != "ol" && doesListContinue(tokens, "\n#", i) {
           results = append(results, "<ol>")
           listType = "ol"
         }
-        results = append(results, "<li>")
+        if listType != "" {
+          results = append(results, "<li>")
+        } else {
+          results = append(results, tokens[i].Val)
+        }
       case tokens[i].Val == "{{":
         body, eidx := parseTemplate(input, tokens, i, mi)
         results = append(results, body)
         i = eidx
-      case tokens[i].Val == "[[":
-        body, eidx := parseInternalLink(input, tokens, i, mi)
+      case len(tokens) > (i+1) && tokens[i].Val == "[" && tokens[i+1].Val == "[":
+        body, eidx := parseInternalLink(input, tokens, i + 1, mi)
         results = append(results, body)
         i = eidx
       case tokens[i].Val == "[":
@@ -316,16 +424,26 @@ func parseGeneral(input []byte, tokens []token, start int, endtokens []string, m
         body, eidx := parseReference(input, tokens, i, mi)
         results = append(results, body)
         i = eidx
+      case tokens[i].Val == "]":
+        // This happens a lot. No biggie.
+        results = append(results, "]")
       default:
         if endtokens != nil {
-          fmt.Printf("Don't know what to do with token \"%s\". endtokens[0] is \"%s\"\n", tokens[i].Val, endtokens[0])
+          fmt.Printf("Don't know what to do with token \"%s\". endtokens is \"%v\"\n", tokens[i].Val, endtokens)
+          fmt.Printf("Tokens[i].Val is: %s\n", tokens[i].Val)
+          fmt.Printf("Tokens[i-1].Val is: %s\n", tokens[i-1].Val)
+          fmt.Printf("Tokens[i-2].Val is: %s\n", tokens[i-2].Val)
+          fmt.Printf("Tokens[i-3].Val is: %s\n", tokens[i-3].Val)
+          fmt.Printf("Tokens[i-4].Val is: %s\n", tokens[i-4].Val)
           fmt.Printf("  Start is: \"%s\"\n", tokens[start].Val)
+          fmt.Printf("  Opener was: %s\n", tokens[start-1].Val)
+          fmt.Printf("  Pre-Opener was: %s\n", tokens[start-2].Val)
         } else {
           fmt.Printf("Don't know what to do with token '%s'. No endtokens\n", tokens[i].Val)
         }
       }
     } else {
-      results = append(results, unparseEntities(string(tokens[i].Val)))
+      results = append(results, parsePlainText(string(tokens[i].Val)))
     }
     i += 1
   }
